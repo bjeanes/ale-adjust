602c4f2b517cc37b41c4844fd9b4f277b67d5963
Bodaniel Jeanes <me@bjeanes.com>
2008-05-16 11:45:03 +1000

    Added tests and made them pass. Should add some more but got other things to do

--- frametime.rb
+++ frametime.rb
@@ -17,6 +17,16 @@ class FrameTime
     end
   end
   
+  def +(timecode)
+    seconds, frames = to_sec_and_frame(timecode)
+    self.add!(seconds, frames)
+  end
+  
+  def -(timecode)
+    seconds, frames = to_sec_and_frame(timecode)
+    self.add!(-seconds, -frames)
+  end
+  
   # this should be named nicer
   def add!(s, f=0)
     @frame = @frame + f
@@ -27,9 +37,28 @@ class FrameTime
   
   # this should be named nicer
   def minus!(s, f=0)
-    self.add(-s, -f)
+    self.add!(-s, -f)
   end
   
   def to_s; @time.strftime("%H:%M:%S:#{@frame.to_s.rjust(2,'0')}"); end
   def inspect; self.to_s; end
+  
+  class << self
+    private :new
+  end
+  
+  protected
+  def to_sec_and_frame(timecode)
+    if timecode =~ /(\d\d):(\d\d):(\d\d):(\d\d)/
+      hours = $1.to_i
+      minutes = $2.to_i
+      seconds = $3.to_i
+      frames = $4.to_i
+      
+      seconds = seconds + minutes*60 + hours*3600
+      return seconds, frames
+    else
+      return 0,0
+    end
+  end
 end
